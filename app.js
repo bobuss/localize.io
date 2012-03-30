012ea2e814637632f328cada2b6fd0552fea77a0
Bertrand TORNIL <bertrand.tornil@gmail.com>
2012-03-30 10:02:36 +0200

    Consistent json responses when an error occurs

--- app.js
+++ app.js
@@ -13,37 +13,47 @@ var server = connect()
   .use(connect.logger('dev'))
   .use(function(req, res){
 
-  	// sanitize
-  	ip = req.url.match(/\b(?:\d{1,3}\.){3}\d{1,3}\b/g);
+    // sanitize
+    ip = req.url.match(/\b(?:\d{1,3}\.){3}\d{1,3}\b/g);
 
-  	// then geoip
-  	if (null != ip) {
-  	
-	  	city.lookup(ip, function(err, d) {
-	    	if (null != d) {
-	    		res.writeHead(200, {
-	  	  		'Content-Type': 'text/plain'
-	  	  	});
-	        res.end(JSON.stringify(d));
-				} else {
-					res.writeHead(404, {
-	  	  		'Content-Type': 'text/plain'
-	  	  	});
-	  	  	res.end('No localization found.');
-				}
-	    });
+    // then geoip
+    if (null != ip) {
 
-	   } else {
+      city.lookup(ip, function(err, data) {
 
-	   	// bad request : no ip found in the URI
-	   	res.writeHead(400, {
-	  	 	'Content-Type': 'text/plain'
-	  	});
-	  	res.end('No IP address found in the URI.');
+        if (null != data) {
 
-	   }
+          res.writeHead(200, {
+            'Content-Type': 'text/plain'
+          });
+          res.end(JSON.stringify(data));
 
-  })
-  .listen(config.http_port);
+        } else {
+
+          res.writeHead(404, {
+            'Content-Type': 'text/plain'
+          });
+          res.end(JSON.stringify({
+            status: 404,
+            message: 'No localization found.'
+          }));
+
+        }
+      });
+
+     } else {
+
+      // bad request : no ip found in the URI
+      res.writeHead(400, {
+        'Content-Type': 'text/plain'
+      });
+      res.end(JSON.stringify({
+        status: 400,
+        message: 'No IP address found in the URI.'
+      }));
+
+    }
+
+  }).listen(config.http_port);
 
 console.log('Server running on port : ' + config.http_port);
